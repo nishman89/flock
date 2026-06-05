7d74fc5a591ccc2b2569543ec3d5af877bde5db0
unknown <nish_mandal@hotmail.com>
2026-06-05 11:58:32 +0100

    updated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE = 'flock-alt-v22';
+const CACHE = 'flock-alt-v23';
 const ASSETS = [
   'index.html','login.html','signup.html','onboarding.html',
   'home.html','flocks.html','roost.html','roosts.html','event.html','my-flocks.html','my-roosts.html','profile.html',
